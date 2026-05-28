c86fb9f6fca21f848bb5d60e2bbbf1da9fe941e9
Crimson-TG <troycadano@gmail.com>
2026-05-28 15:08:37 +0000

    Added LoanRecord code 1.0

--- SmartLibrarySystem/src/com/library/model/LoanRecord.java
+++ SmartLibrarySystem/src/com/library/model/LoanRecord.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.library.model;
 
-/**
- *
- * @author Administrator
- */
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class LoanRecord {
+    private String bookId;
+    private String borrowerName;
+    private LocalDate borrowDate;
+    private LocalDate dueDate;
+    private LocalDate returnedDate;
+
+    public LoanRecord(String bookId, String borrowerName) {
+        this.bookId = bookId;
+        this.borrowerName = borrowerName;
+        this.borrowDate = LocalDate.now();
+        this.dueDate = this.borrowDate.plusDays(7);
+        this.returnedDate = null;
+    }
+
+    public double calculatePenalty() {
+        LocalDate endDate = (returnedDate != null) ? returnedDate : LocalDate.now();
+        if (endDate.isAfter(dueDate)) {
+            long daysOverdue = ChronoUnit.DAYS.between(dueDate, endDate);
+            return daysOverdue * 10.0;
+        }
+        return 0.0;
+    }
+
+    public String getBookId() { return bookId; }
+    public String getBorrowerName() { return borrowerName; }
+    public LocalDate getBorrowDate() { return borrowDate; }
+    public LocalDate getDueDate() { return dueDate; }
+    public LocalDate getReturnedDate() { return returnedDate; }
     
+    public void setReturnedDate(LocalDate returnedDate) { 
+        this.returnedDate = returnedDate; 
+    }
 }
